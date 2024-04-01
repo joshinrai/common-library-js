1dbe3670a1e4fc6271ab415587dddfae672bf9bc
joshinrai <joshinrai@163.com>
2024-04-01 17:53:36 +0800

    Update graphFormat.js

--- src/graphFormat.js
+++ src/graphFormat.js
@@ -1,12 +1,3 @@
-/* eslint-disable no-param-reassign */
-/*
- * @Author: xushenlei joshinrai@163.com
- * @Date: 2023-12-29 11:25:13
- * @LastEditors: xushenlei joshinrai@163.com
- * @LastEditTime: 2024-01-22 14:06:07
- * @FilePath: /ddw-development-web/src/pages/DataDevelopment/pages/NewWarehouseDesign/components/flow/graphFormat.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import cloneDeep from 'lodash/cloneDeep';
 import G6 from '@antv/g6';
 
